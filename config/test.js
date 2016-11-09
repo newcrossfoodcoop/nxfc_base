fbee578f35fac8577a359a9600ff70c7ef8206f0
Ben Simpson <ben@hy-street.net>
2016-11-09 15:37:26 +0000

    add default express test config

--- config/test.js
+++ config/test.js
@@ -4,5 +4,12 @@ module.exports = {
     nodeEnvShort: 'test',
     // Any services provided should be given different port numbers for test
 	provides: {
+		express: {
+	        logging: null,
+            port: 8081,
+            externalUrl: {
+                host: 'localhost:8081'
+            },
+        }
 	}
 };
